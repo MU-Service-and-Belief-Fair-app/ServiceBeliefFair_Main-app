ae27fce100dbb2aa9b77c01f303595f350c4334c
Kyle <kyleoris@kyles-macbook-pro.local>
2018-04-10 19:02:42 -0500

    Initial Commit

---
+++ ServiceBeliefFair/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServiceBeliefFair
+//
+//  Created by Kyle Oris on 4/10/18.
+//  Copyright © 2018 Kyle Oris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
